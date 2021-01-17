72249216ff1bbc71a7f69edcfac6f1fa698d7fd6
JDemosthene <JDemosthene@conveyhs.com>
2021-01-17 04:54:54 -0500

    Initial Code

---
+++ API/Startup.cs
@@ -0,0 +1,46 @@
+﻿using INFRASTRUCTURE.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration config)
+        {
+           Configuration = config;
+        }
+
+     
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<StoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnectionString")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ INFRASTRUCTURE/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using CORE.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace INFRASTRUCTURE.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
